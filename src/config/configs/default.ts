7b20debab16b9484e42a9114bdb5fbd8f4088729
Julio Sansossio <jsansossio@aluxion.com>
2019-04-25 15:55:11 +0200

    useless config

--- src/config/configs/default.ts
+++ src/config/configs/default.ts
@@ -5,17 +5,4 @@ export default {
   application: {
     port: process.env.PORT || 3000,
   },
-  database: {
-    postgres: {
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: ['{src,dist}/**/**.entity{.ts,.js}'],
-      synchronize: true,
-      logging: false,
-    },
-  },
 };
