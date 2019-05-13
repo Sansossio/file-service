df91120b0e039b993a492dbd1abce35be32de65e
Julio Sansossio <jsansossio@aluxion.com>
2019-05-13 14:47:21 +0200

    handlerbars error handler

--- src/handlebars/handlebars.service.ts
+++ src/handlebars/handlebars.service.ts
@@ -1,13 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { compile, TemplateDelegate } from 'handlebars';
 import { CompileHandlebarsDtoResponse } from './dto/compile.handlebars.dto';
 
 @Injectable()
 export class HandlebarsService {
   compile(hbs: string, data: any): CompileHandlebarsDtoResponse {
-    const template: TemplateDelegate = compile(hbs);
-    return {
-      html: template(data),
-    };
+    try {
+      const template: TemplateDelegate = compile(hbs);
+      return {
+        html: template(data),
+      };
+    } catch (e) {
+      delete e.stack;
+      throw new HttpException(e, 500);
+    }
   }
 }
