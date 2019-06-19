2800673893ed2b249caf027d0ebafaacb1d3556c
Julio Sansossio <jsansossio@aluxion.com>
2019-06-19 10:16:01 +0200

    encodeURI

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -3,14 +3,17 @@ import * as rp from 'request-promise';
 
 @Injectable()
 export class FileService {
-  async get(uri: string): Promise<Buffer> {
+  async get(uri: string, repeat: boolean = true): Promise<Buffer> {
     try {
       const options: rp.OptionsWithUri = {
-        uri: encodeURI(uri),
+        uri,
         encoding: null,
       };
       return (await (rp(options) as any)) as Buffer;
     } catch (e) {
+      if (repeat) {
+        return await this.get(encodeURI(uri), false);
+      }
       throw new NotFoundException(`Pdf not found: ${uri}`);
     }
   }
